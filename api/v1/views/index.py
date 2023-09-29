b593d5edd83903f4050234b33699ee8f4aa498d7
Fayth7 <flexflakes@gmail.com>
2023-09-29 18:58:44 +0300

    task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,17 @@
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-        """Return the status of the API."""
-            return jsonify({"status": "OK"})
-        
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Retrieve the number of objects by type."""
+    stats_dict = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(stats_dict)
