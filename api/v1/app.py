6073becc19b086c25915c3e3cbb05bb8cabbc1ad
Fayth7 <flexflakes@gmail.com>
2023-09-29 19:16:12 +0300

    error handler

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,6 +13,12 @@ def teardown_appcontext(exception):
     """Closes the storage on teardown."""
     storage.close()
 
+
+@app.errorhandler(404)
+def not_found(error):
+    """Custom 404 error handler."""
+    return jsonify({"error": "Not found"}), 404
+
     if __name__ == "__main__":
         host = os.getenv("HBNB_API_HOST", "0.0.0.0")
         port = int(os.getenv("HBNB_API_PORT", 5000))
