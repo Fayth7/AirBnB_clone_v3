b40182a887c25bdcea684a68d29f5ca25e24c98d
Fayth7 <flexflakes@gmail.com>
2023-10-02 04:00:28 -0700

    pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
                 jo = json.load(f)
             for key in jo:
                 self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
-        except:
+        except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
@@ -79,8 +79,3 @@ class FileStorage:
         if cls is None:
             return len(self.all())
         return len(self.all(cls))
-
-
-
-
-
