43c60a5b1b8fc3aa56444c964c7eabd3779e835c
Fayth7 <flexflakes@gmail.com>
2023-10-02 04:18:52 -0700

    permissions

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,7 +23,13 @@ def not_found(error):
     """Custom 404 error handler."""
     return jsonify({"error": "Not found"}), 404
 
-    if __name__ == "__main__":
-        port = int(os.getenv("HBNB_API_PORT", 5000))
-        host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-        app.run(host=host, port=port, threaded=True)
+    if __name__ == '__main__':
+        if getenv("HBNB_API_HOST") is None:
+            HBNB_API_HOST = '0.0.0.0'
+        else:
+            HBNB_API_HOST = getenv("HBNB_API_HOST")
+        if getenv("HBNB_API_PORT") is None:
+            HBNB_API_PORT = 5000
+        else:
+            HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+        app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
