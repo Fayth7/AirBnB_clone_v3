0c3900af0462e908cba5b765d13a11ca1aeca910
Fayth7 <flexflakes@gmail.com>
2023-10-02 03:01:47 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,11 +8,8 @@ from os import getenv
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
-
-# Register the blueprint
+cors = CORS(app, resources={r"api/v1/*": {"origin": "*"}})
 app.register_blueprint(app_views)
-app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
@@ -27,12 +24,6 @@ def not_found(error):
     return jsonify({"error": "Not found"}), 404
 
     if __name__ == "__main__":
-        if getenv("HBNB_API_HOST") is None:
-            HBNB_API_HOST = '0.0.0.0'
-        else:
-            HBNB_API_HOST = getenv("HBNB_API_HOST")
-        if getenv("HBNB_API_PORT") is None:
-            HBNB_API_PORT = 5000
-        else:
-            HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
-        app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
+        port = int(os.getenv("HBNB_API_PORT", 5000))
+        host = os.getenv("HBNB_API_HOST", "0.0.0.0")
+        app.run(host=host, port=port, threaded=True)
